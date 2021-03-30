d35383e57da05cf13cb9858035522bcb901d9b58
adoherty8 <azdoherty91@gmail.com>
2021-03-29 21:24:22 -0400

    background and equal h/w

--- app.py
+++ app.py
@@ -19,6 +19,38 @@ rchart.download_starting_data()
 rchart.process()
 
 
+
+chart_background = [
+    go.Scatter(
+        x=[97, 100, 100, 97, 97],
+        y=[97, 97, 100, 100, 97],
+        fill='toself',
+        fillcolor='rgba(255, 235, 235, 255)',
+        line=dict(color='rgba(255, 235, 235, 255)'),
+    ),
+    go.Scatter(
+        x=[97, 100, 100, 97, 97],
+        y=[100, 100, 103, 103, 100],
+        fill='toself',
+        fillcolor='rgba(235,235,255,255)',
+        line=dict(color='rgba(235,235,255,255)'),
+    ),
+    go.Scatter(
+        x=[100, 103, 103, 100, 100],
+        y=[97, 97, 100, 100, 97],
+        fill='toself',
+        fillcolor='rgba(255,255,235,255)',
+        line=dict(color='rgba(255,255,235,255)'),
+    ),
+    go.Scatter(
+        x=[100, 103, 103, 100, 100],
+        y=[100, 100, 103, 103, 100],
+        fill='toself',
+        fillcolor='rgba(235,245,235,255)',
+        line=dict(color='rgba(235,245,235,255)'),
+    )
+]
+
 app.layout = html.Div(
     children=[
         html.H1('Rotation Chart'),
@@ -42,16 +74,21 @@ app.layout = html.Div(
         dcc.Graph(
             figure={
                 'data':
-                    [
+                    chart_background + [
                         go.Scatter(
                             x=rchart.data.loc[:, ('JDK RS-ratio', c[1])],
                             y=rchart.data.loc[:, ('JDK RS-Momentum', c[1])],
                             hovertext=rchart.data.index,
                             mode="markers+lines",
-                            name=c[1]
+                            name=c[1],
                         ) for c in [('RS-Ratio', 'XLE'), ('RS-Ratio', 'QQQ')]
                     ],
+                'layout': dict(
+                    height=800,
+                    width=800
+                )
             }
+
         )
     ]
 )
